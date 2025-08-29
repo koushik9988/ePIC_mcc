f56c62475d58e36d68d370180bce392b2349db29
Kaushik Kalita <55924787+koushik9988@users.noreply.github.com>
2025-08-29 18:56:28 +0530

    Delete python_scripts/extra directory

--- python_scripts/extra/run_all_script.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-
-scripts = ["momentum_plot.py", "ke_plot.py"] 
-
-for script in scripts:
-    for i in range(1, 13):
-        arg = f"../data_run_{i}"
-        subprocess.run(["python3", script, arg])
